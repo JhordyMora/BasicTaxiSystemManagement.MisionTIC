f0b08b22062f30967f557374502113d04831577f
jhordymora <jhordy.mora@web.de>
2021-09-07 14:35:00 +0200

    Progress model

---
+++ BasicTaxiSystemManagement.MisionTIC/src/Model/Vehiculo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author jhoal
+ */
+public class Vehiculo {
+    String veh_placa;
+    String veh_marca;
+    String veh_modelo;
+    int veh_anhio;
+    int veh_capacidad;
+    String veh_color;
+    int veh_kilometros;
+    
+}
