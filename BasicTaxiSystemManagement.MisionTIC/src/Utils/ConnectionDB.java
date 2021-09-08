d649e0704ec56357d4c2cd122162f5d0cad0e486
jhordymora <jhordy.mora@web.de>
2021-09-08 20:17:46 +0200

    Conection DB

--- BasicTaxiSystemManagement.MisionTIC/src/Utils/ConnectionDB.java
+++ BasicTaxiSystemManagement.MisionTIC/src/Utils/ConnectionDB.java
@@ -16,5 +16,28 @@ public class ConnectionDB {
     public static Connection getConnection(){
         JSONParser parser = new JSONParser();
         Connection conn = null;
+        try{
+            String credentials_path = System.getProperty("user.dir") + "/src/Utils/db-credentials.json";
+            JSONObject jsonObject  = (JSONObject)parser.parse(new FileReader(credentials_path));
+            
+            String host  = (String)jsonObject.get("db_ip");
+            String port = (String)jsonObject.get("dp_port");
+            String username = (String)jsonObject.get("db_user");
+            String password = (String)jsonObject.get("db_password");
+            String dbURL = "jdbc:mysql://"+host+":"+port+"/taxis";
+            conn = DriverManager.getConnection(dbURL , username , password );
+            
+            if (conn != null) {
+                System . out. println (" Conectado ");
+            }
+        } catch (SQLException | IOException | ParseException ex){
+            ex.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        getConnection();  
     }
 }
