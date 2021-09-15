51f9083cb42fc578fddec259b881661c5073a802
jhordymora <jhordy.mora@web.de>
2021-09-15 18:15:22 +0200

    Changes and advances in View

--- BasicTaxiSystemManagement.MisionTIC/src/View/Canvas.java
+++ BasicTaxiSystemManagement.MisionTIC/src/View/Canvas.java
@@ -1,7 +1,14 @@
 package View;
 
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Canvas extends JPanel{
-    
+    public Canvas(){
+        setLayout(new BorderLayout());
+        add(new JButton("Prueba0"), BorderLayout.WEST);
+        add(new JButton("Prueba1"), BorderLayout.CENTER);
+        add(new JButton("Prueba2"), BorderLayout.EAST);
+    }
 }
